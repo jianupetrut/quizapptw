af082a0cca06f828bf3293e840cd2b3473ffc660
Jianu_Petrut <c.petrut97@gmail.com>
2018-11-23 13:58:58 +0200

    added android skeleton

---
+++ QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/JoinTestActivity.java
@@ -0,0 +1,13 @@
+package ro.ase.codinquiz.quizapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_test);
+    }
+}
