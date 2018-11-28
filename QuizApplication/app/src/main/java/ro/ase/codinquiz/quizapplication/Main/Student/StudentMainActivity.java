bc5632f98b4194cc1e3a08d70d49e95a229487a4
Jianu_Petrut <c.petrut97@gmail.com>
2018-11-28 14:32:26 +0200

    package changes

--- QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Student/StudentMainActivity.java
+++ QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Student/StudentMainActivity.java
@@ -1,4 +1,4 @@
-package ro.ase.codinquiz.quizapplication.Student;
+package ro.ase.codinquiz.quizapplication.Main.Student;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
