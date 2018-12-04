0bffdefff4e1fb37bbd4ee972d25581b5df78934
andreeastroe <32742434+andreeastroe@users.noreply.github.com>
2018-12-04 20:39:16 +0200

    added activity_generate_code and activity_test_options

--- QuizApplication/main/java/ro/ase/codinquiz/quizapplication/Main/Login/LoginActivity.java
+++ QuizApplication/main/java/ro/ase/codinquiz/quizapplication/Main/Login/LoginActivity.java
@@ -1,12 +1,8 @@
 package ro.ase.codinquiz.quizapplication.Main.Login;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-import ro.ase.codinquiz.quizapplication.Main.Student.JoinTestActivity;
-import ro.ase.codinquiz.quizapplication.Main.Student.StudentMain;
 import ro.ase.codinquiz.quizapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,13 +11,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-    public void joinTest(View view){
-        Intent intent = new Intent(this, JoinTestActivity.class);
-        startActivity(intent);
-    }
-    public void openStudentActivity(View view){
-       Intent intent =new Intent(this,StudentMain.class);
-       startActivity(intent);
     }
 }
