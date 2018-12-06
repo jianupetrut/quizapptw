6ecc8ba2856a891ea842f582fab6d245b2623709
constantindoinita16 <constantindoinita@gmail.com>
2018-12-06 20:23:16 +0200

    Added StudentTestActivity and StudentStartTestActivity and linked them to  JoinTest. Attempted to use a fragment for question display in test.

---
+++ QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Student/StudentStartTestActivity.java
@@ -0,0 +1,13 @@
+package ro.ase.codinquiz.quizapplication.Main.Student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentStartTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_start_test);
+    }
+}
---
+++ QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Student/StudentTestActivity.java
@@ -0,0 +1,13 @@
+package ro.ase.codinquiz.quizapplication.Main.Student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_test);
+    }
+}
