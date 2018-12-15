6c0bf13b2a9a6e1377d5333d5b5f9f0aac98907d
George Dobrin <georgedobrin11@gmail.com>
2018-12-15 23:02:47 +0200

    React project structure + SemanticUI imported

---
+++ app/src/components/Login.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './Login.css';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+
+class Login extends Component{
+    render(){
+        return(
+            <p>Salutare!!</p>
+        )
+    }
+}
+
+const FormExampleForm = () => (
+    <div>
+        <Login></Login>
+        <Form>
+        <Form.Field>
+            <label>First Name</label>
+            <input placeholder='First Name' />
+        </Form.Field>
+        <Form.Field>
+            <label>Last Name</label>
+            <input placeholder='Last Name' />
+        </Form.Field>
+        <Form.Field>
+            <Checkbox label='I agree to the Terms and Conditions' />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+        </Form>
+    </div>
+  )
+  
+  export default FormExampleForm;
