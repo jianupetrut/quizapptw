5ceec8c293d8cd765a12f3a3c747666ba6dbb6b4
Andreea Stroe <aandreeastroe@yahoo.com>
2019-01-12 23:07:43 +0200

    d

--- QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Teacher/TeacherExistingTests.java
+++ QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Teacher/TeacherExistingTests.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
