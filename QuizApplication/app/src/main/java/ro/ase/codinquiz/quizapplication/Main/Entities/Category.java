5b0d62a0f64b01161bdfd197fd3f06a918eaecc8
Jianu_Petrut <c.petrut97@gmail.com>
2019-01-13 03:54:24 +0200

    modified entities,added get api requests

--- QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Entities/Category.java
+++ QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Entities/Category.java
@@ -3,4 +3,23 @@ package ro.ase.codinquiz.quizapplication.Main.Entities;
 public class Category {
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public Category(int id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
 }
--- QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Entities/FinishedTest.java
+++ QuizApplication/app/src/main/java/ro/ase/codinquiz/quizapplication/Main/Entities/FinishedTest.java
@@ -6,6 +6,39 @@ public class FinishedTest {
     private int id;
     private int test_id;
     private float score;
+    private String username;
+    private String testName;
+    private Date date;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public FinishedTest(int id, int test_id, float score, String username, String testName, Date date, Date date1) {
+
+        this.id = id;
+        this.test_id = test_id;
+        this.score = score;
+        this.username = username;
+        this.testName = testName;
+        this.date = date;
+        this.date = date1;
+    }
+
+    public FinishedTest() {
+    }
 
     public int getId() {
         return id;
@@ -39,5 +72,5 @@ public class FinishedTest {
         this.date = date;
     }
 
-    private Date date;
+
 }
